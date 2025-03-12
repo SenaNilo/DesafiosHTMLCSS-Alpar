b0a3ecf9d93eb051d14a2e15295df8e65306c9ef
SenaNilo <danilo.sena005@gmail.com>
2025-03-12 10:44:36 -0300

    Aula 1 e 2 de JS

---
+++ blog/assets/js/aulas.js
@@ -0,0 +1,15 @@
+console.log("Olá mundo externamente")
+// window = Objeto global do JS
+
+const menuEl = document.getElementById("header");
+const postsEls = document.getElementsByClassName("post");
+const linksEls = document.getElementsByTagName("a");
+const buttonEl = document.querySelector("#header button");
+const paragraphsEls = document.querySelectorAll(".post p");
+
+
+console.dir(menuEl)
+console.dir(postsEls)
+console.dir(linksEls)
+console.dir(buttonEl)
+console.dir(paragraphsEls)
