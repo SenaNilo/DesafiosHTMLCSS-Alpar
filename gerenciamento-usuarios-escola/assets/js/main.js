684cc9bc55971a042bf281ed94897ce88e80955a
SenaNilo <danilo.sena005@gmail.com>
2025-04-25 13:37:01 -0300

    Desafio 4, inicio da construcao de um app AngularJS

---
+++ gerenciamento-usuarios-escola/assets/js/main.js
@@ -0,0 +1,9 @@
+const app = angular.module("managerModule" , [])
+
+app.controller("AppController", function ($scope) {
+    $scope.mensagem = "Bem-vindo ao sistema de cadastro escolar";
+    $scope.usuario = {
+        nome: "João",
+        tipo: "Aluno"
+    }
+})
